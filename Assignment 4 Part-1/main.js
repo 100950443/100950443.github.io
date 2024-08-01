52b1f3f718679cd6cbe6422844873edc264556ce
100950443 <sneh.patel9@dcmail.ca>
2024-08-01 19:55:51 -0400

    Commit-5
    added the const code with comments in it.

--- Assignment 4 Part-1/main.js
+++ Assignment 4 Part-1/main.js
@@ -3,3 +3,14 @@ Name: Sneh patel
 File: Index(6).html
 Date: 01 August 2024
 /*Description: Generates a silly story when the "Generate random story" button is pressed.*/
+
+
+// Get the element with the ID
+const customName = document.getElementById('customname');
+// Get the first element with the class 'randomize' and assign it to variable 'randomize'
+const randomize = document.querySelector('.randomize');
+// Get the first element with the class 'sotry' and assign it to variable 'story'
+const story = document.querySelector('.story');
+function randomValueFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
